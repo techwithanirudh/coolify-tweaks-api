503007ea446fc13ca76a1f20e661a2e24576ef40
Anirudh Sriram <hello@techwithanirudh.com>
2025-07-15 16:45:29 +0000

    feat: restore compatibility date in Nitro configuration

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,6 +2,7 @@ import { defineNitroConfig } from 'nitropack/config';
 
 export default defineNitroConfig({
   srcDir: 'server',
+  compatibilityDate: '2025-07-15',
   imports: {
     autoImport: false,
   },
